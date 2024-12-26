2f6fe31afb823e0455db63b2ca2b1d43e13fcbea
arunprajapati-arch <arunprajapati.mail@gmail.com>
2024-12-26 18:12:21 +0530

    initialised next app with shadcn

---
+++ src/providers/providers.tsx
@@ -0,0 +1,18 @@
+'use client';
+import React from 'react';
+import { ThemeProvider } from './theme-provider';
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    
+       <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          > 
+        {children}
+      </ThemeProvider>
+    
+  );
+};
