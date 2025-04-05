a96bddfdeadebdd07a2de073013f8a253334866e
arunprajapati-arch <arunprajapati.mail@gmail.com>
2025-04-05 18:36:10 +0530

    init repo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,17 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@workspace/ui/components/avatar"
 import { Button } from "@workspace/ui/components/button"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
+      <Avatar>
+  <AvatarImage src="https://github.com/shadcn.png" />
+  <AvatarFallback>CN</AvatarFallback>
+</Avatar>
+
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Button >Button</Button>
       </div>
     </div>
   )
